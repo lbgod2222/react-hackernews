b44dc363028e0f11524895adb42fa86c38601fe3
lbgod2222 <1097843969@qq.com>
2019-09-29 08:23:04 +0800

    :anchor: Stage: init pack format

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/images/logo.svg';
+import '../assets/style/App.css';
 
 function App() {
   return (
